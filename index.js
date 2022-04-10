9684ae28ef81154361dc38702536da45ec510fe4
Shanna Smith <shannagjh@gmail.com>
2022-04-10 16:22:54 -0400

    use port env

--- index.js
+++ index.js
@@ -128,6 +128,5 @@ app.get('/account/all', function (req, res) {
 
 
 
-var port = 3000;
-app.listen(port);
-console.log('Running on port: ' + port);
+var port = process.env.PORT || 3000;
+app.listen(port, () => console.log('Running on port: ' + port));
